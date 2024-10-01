3b30d60465361f27037494b13288ae3d3d3e136d
Daryl Echeazu <darylecheazu@gmail.com>
2024-10-01 14:19:33 -0500

    LS added for Yield Strength Prediction Graphs

--- copper_alloy_YS_.02.py
+++ copper_alloy_YS_.02.py
@@ -83,7 +83,7 @@ plt.scatter(y_test, y_pred, label="Predicted Values", color='blue', edgecolors='
 plt.plot(y_test, y_test, color='black', linestyle='--', label='Parity Line')  # Plot parity line
 plt.xlabel("Experimental Yield Strength (YS) [MPa]", fontsize=14)
 plt.ylabel("Predicted Yield Strength (YS) [MPa]", fontsize=14)
-plt.title("Yield Strength Prediction for Copper Alloys", fontsize=16)
+plt.title("Yield Strength Prediction for Copper Alloys (0.02 LS)", fontsize=16)
 plt.text(0.05, 0.85, f'MAE: {mae:.2f} MPa', transform=plt.gca().transAxes, fontsize=12)
 plt.text(0.05, 0.80, f'τ: {tau:.2f}', transform=plt.gca().transAxes, fontsize=12)
 plt.text(0.05, 0.75, f'ρ: {spearman:.2f}', transform=plt.gca().transAxes, fontsize=12) 
